ca64a048dae34f16ccd3927eaf2c61ee88962613
Pavan Kumar Jadda <jaddap2@nih.gov>
2019-11-21 03:00:38 -0500

    First Commit

---
+++ src/main/java/com/pj/vaultdemo/VaultdemoApplication.java
@@ -0,0 +1,15 @@
+package com.pj.vaultdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultdemoApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(VaultdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pj/vaultdemo/VaultdemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pj.vaultdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultdemoApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
